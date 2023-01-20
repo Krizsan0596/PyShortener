a0d02753a57298d5e22e8198a5cc60789f86f675
Krizsan0596 <kristof.nagy.lazar@gmail.com>
2023-01-20 15:40:29 +0100

    Fixed errors

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ import string
 import secrets
 import pickle
 import hashlib
+import atexit
+from urllib.parse import urlsplit
 from flask import Flask, request, redirect, render_template, flash, request
 
 
 app = Flask(__name__)
 chars = string.ascii_letters + string.digits + '-'
 length = 8
+format = 'utf-8'
+app.secret_key = ''.join(secrets.choice(chars) for i in range(length))
 
+def save_links():
+    with open("links.pkl", "wb") as file:
+        pickle.dump(links, file)
+atexit.register(save_links)
 
 if os.path.exists('links.pkl'):
     with open('links.pkl', 'rb') as file:
@@ -21,7 +29,7 @@ else:
 
 def hash(input):
     hash_obj = hashlib.md5()
-    hash_obj.update(bytes(input))
+    hash_obj.update(bytes(input, format))
     hashed = hash_obj.hexdigest()
     return hashed
 
@@ -30,23 +38,27 @@ def generate_link():
     if request.method == 'POST':
         link = request.form['input_link']
         if len(str(link)) > 2000:
-            flash("Url too long. Perhaps try a shorter one.")
-            return redirect(request.url)
-        is_custom_link = request.form['custom_link_box']
+            return "Url too long. Perhaps try a shorter one."
+        try:
+            is_custom_link = request.form['custom_link_box']
+        except KeyError:
+            is_custom_link = False
         if is_custom_link:
             custom_link = request.form['custom_link']
             if hash(custom_link) in links:
-                flash("Custom link already in use.")
-                return redirect(request.url)
+                return "Custom link already in use."
             else:
                 short = custom_link
         else:
             short = ''.join(secrets.choice(chars) for i in range(length))
-        if not link.startswith(("http://", "https://")):
-            link = "http://" + link
+        parsed_link = urlsplit(link)
+        if parsed_link.scheme == '':
+            link = 'https://' + link
+        elif parsed_link.scheme != 'http' and parsed_link.scheme != 'https':
+            return 'Invalid link. Only \'http(s)\' links are supported.'
         links[hash(short)] = link
         print(short)
-        return link + '\\\\' + short
+        return link + '\\\\ Your short link is: ' + short
     return render_template('index.html')
 
 
@@ -58,9 +70,5 @@ def main(link):
         return "Short link does not exists, are you sure you entered the right one?"
 
 
-try:
-    if __name__ == '__main__':
-        app.run(port=8181, debug=True)
-except KeyboardInterrupt:
-    with open("links.pkl", "wb") as file:
-        pickle.dump(links, file)
+if __name__ == '__main__':
+    app.run(port=8181, debug=True)
